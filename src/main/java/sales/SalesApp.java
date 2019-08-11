bbc94d019f3149045d983282553109f0cb8d639b
xjhxcc <2351889342@qq.com>
2019-08-11 22:00:13 +0800

    Refactor:extract method

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -11,16 +11,43 @@ public class SalesApp {
 		if (salesId == null) {
 			return;
 		}
-		SalesDao salesDao = new SalesDao();
-		Sales sales = salesDao.getSalesBySalesId(salesId);
-		
-		Date today = new Date();
-		if (today.after(sales.getEffectiveTo())
-				|| today.before(sales.getEffectiveFrom())){
-			return;
+		Sales sales = getSales(salesId);
+
+		if (getEffective(sales)) return;
+		List<SalesReportData> reportDataList = getSalesReportData(sales);
+		List<SalesReportData> filteredReportDataList = getFilteredReportDataList(isSupervisor, reportDataList);
+
+
+		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
+		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
+			tempList.add(reportDataList.get(i));
 		}
+		filteredReportDataList = tempList;
+		List<String> headers = getHeaders(isNatTrade);
+
+		SalesActivityReport report = this.generateReport(headers, reportDataList);
+		
+		EcmService ecmService = new EcmService();
+		ecmService.uploadDocument(report.toXml());
+		
+	}
+
+	private List<String> getHeaders(boolean isNatTrade) {
+		List<String> headers = null;
+		if (isNatTrade) {
+			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
+		} else {
+			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
+		}
+		return headers;
+	}
+
+	private List<SalesReportData> getSalesReportData(Sales sales) {
 		SalesReportDao salesReportDao = new SalesReportDao();
-		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
+		return salesReportDao.getReportData(sales);
+	}
+
+	private List<SalesReportData> getFilteredReportDataList(boolean isSupervisor, List<SalesReportData> reportDataList) {
 		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
 		for (SalesReportData data : reportDataList) {
 			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
@@ -33,24 +60,21 @@ public class SalesApp {
 				}
 			}
 		}
-		
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
+		return filteredReportDataList;
+	}
+
+	private boolean getEffective(Sales sales) {
+		Date today = new Date();
+		if (today.after(sales.getEffectiveTo())
+				|| today.before(sales.getEffectiveFrom())){
+			return true;
 		}
-		filteredReportDataList = tempList;
-		List<String> headers = null;
-		if (isNatTrade) {
-			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
-		} else {
-			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
-		}
-		
-		SalesActivityReport report = this.generateReport(headers, reportDataList);
-		
-		EcmService ecmService = new EcmService();
-		ecmService.uploadDocument(report.toXml());
-		
+		return false;
+	}
+
+	private Sales getSales(String salesId) {
+		SalesDao salesDao = new SalesDao();
+		return salesDao.getSalesBySalesId(salesId);
 	}
 
 	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
